17814ee18dcba68342bbd373307d752320915f2c
Lachezar Boyadzhiev <l.boyadzhievv@gmail.com>
2024-09-10 18:34:00 +0300

    Accordian app finished

--- src/components/Accordian/Accordian.tsx
+++ src/components/Accordian/Accordian.tsx
@@ -7,28 +7,55 @@ import reactQuestions from './data';
 import classes from './Accordian.module.css';
 
 export default function Accordian() {
-  const [selected, setSelected] = useState<number | null>(null);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [isMultiSelection, setIsMultiSelection] = useState(false);
 
-  function handleSingleSelection(currentId: number) {
-    setSelected(currentId === selected ? null : currentId);
+  function handleEnableMultiSelection() {
+    setIsMultiSelection((state) => !state);
+
+    if (isMultiSelection && selectedItems.length > 1) setSelectedItems([]);
+  }
+
+  function handleSelection(currentId: number) {
+    let selected = [...selectedItems];
+
+    const indexOfCurrenctId = selected.indexOf(currentId);
+
+    if (indexOfCurrenctId !== -1) {
+      isMultiSelection
+        ? selected.splice(indexOfCurrenctId, 1)
+        : (selected = []);
+    } else {
+      isMultiSelection ? selected.push(currentId) : (selected = [currentId]);
+    }
+
+    setSelectedItems(selected);
   }
 
   return (
     <div className={classes.wrapper}>
+      <button
+        onClick={handleEnableMultiSelection}
+        className={`${classes.selectBtn} ${
+          isMultiSelection ? classes.mlctBtn : ''
+        }`}
+      >
+        Enable Multi Selection
+      </button>
       <div className={classes.accordian}>
         {reactQuestions && reactQuestions.length > 0 ? (
           reactQuestions.map((item) => (
             <div
               key={item.id}
               className={classes.item}
-              onClick={() => handleSingleSelection(item.id)}
+              onClick={() => handleSelection(item.id)}
             >
               <div className={classes.title}>
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
 
-              {selected === item.id ? (
+              {selectedItems.includes(item.id) ? (
                 <div className={classes.content}>{item.answer}</div>
               ) : null}
             </div>
