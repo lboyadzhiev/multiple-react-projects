1158ce325af0a8902fced20ad9f53b9843854db1
Lachezar Boyadzhiev <l.boyadzhievv@gmail.com>
2024-09-11 18:49:08 +0300

    Generate Color App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Accordian from './components/Accordian/Accordian';
+import RandomColor from './components/RandomColor/RandomColor';
 
 function App() {
   return (
     <div className='App'>
       <Accordian />
+      <RandomColor />
     </div>
   );
 }
---
+++ src/components/RandomColor/RandomColor.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+// styles
+import classes from './RandomColor.module.css';
+
+export default function RandomColor() {
+  const [color, setColor] = useState('#000000');
+
+  function generateIndex(length: number) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function generateColorHandler(typeOfColor: string) {
+    if (typeOfColor === 'hex') {
+      const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+      let hexColor = '#';
+
+      for (let i = 0; i < 6; i++) {
+        hexColor += hex[generateIndex(hex.length)];
+      }
+
+      setColor(hexColor);
+    } else if (typeOfColor === 'rgb') {
+      const r = generateIndex(256);
+      const g = generateIndex(256);
+      const b = generateIndex(256);
+
+      setColor(`rgb(${r}, ${g}, ${b})`);
+    }
+  }
+
+  return (
+    <div className={classes.container} style={{ background: color }}>
+      <div className={classes.btnContainer}>
+        <button onClick={() => generateColorHandler('hex')}>
+          Create HEX Color
+        </button>
+        <button onClick={() => generateColorHandler('rgb')}>
+          Create RGB Color
+        </button>
+      </div>
+
+      <div className={classes.colorString}>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+}
